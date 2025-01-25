cfb6b4cce1c38da7ae2bf86044d9d177a046e8e9
Akhilesh <akhilesh082003@gmail.com>
2025-01-25 17:48:42 +0530

    add the routes in the apllication

--- backend/api/src/routes/order.ts
+++ backend/api/src/routes/order.ts
@@ -1,9 +1,45 @@
 import { Hono } from "hono";
+import { RedisManager } from "../Redismanager";
 
 const order = new Hono()
 
-order.post("/", (c) => {
-  return c.text("this the order router")
+order.post("/", async (c) => {
+  const { price, market, side, quantity, userId } = await c.req.json()
+  const response = await RedisManager.getInstance().getReponseForOrderPlaced({
+    type: "CREATE_ORDER",
+    data: {
+      price,
+      market,
+      side,
+      quantity,
+      userId
+    }
+  })
+  console.log(response)
+})
+
+order.delete("/", async (c) => {
+  const { market, orderId } = await c.req.json()
+  const response = await RedisManager.getInstance().getReponseForOrderPlaced({
+    type: "CANCEL_ORDER",
+    data: {
+      market,
+      orderId
+    }
+  })
+  console.log(response)
+})
+
+order.get("/open", async (c) => {
+  const { userId, market } = await c.req.json()
+  const response = await RedisManager.getInstance().getReponseForOrderPlaced({
+    type: "OPEN_ORDER",
+    data: {
+      market,
+      userId
+    }
+  })
+  console.log(response)
 })
 
 
